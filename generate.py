2b340d768946522e9d1d67afa793bd2e8d2595c9
Markus Reichold <reichold.markus@gmx.de>
2025-09-29 13:51:27 +0200

    V9.8: Changed generate.py script to generate more images without vram problems

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 # https://stackoverflow.com/questions/52839758/matplotlib-and-runtimeerror-main-thread-is-not-in-main-loop
 # https://stackoverflow.com/questions/66541812/kochat-in-use-runtimeerror-main-thread-is-not-in-main-loop
 # https://stackoverflow.com/questions/27147300/matplotlib-tcl-asyncdelete-async-handler-deleted-by-the-wrong-thread
+
 import os
 from prettytable import PrettyTable
 import torch
@@ -98,6 +99,50 @@ class Generate():
             # Save the PIL image as .png fole
             img.save(img_pth)
 
+    # NEW: Generate and save images one by one to avoid VRAM issues
+    def generate_and_save_images_one_by_one(self, num_samples, img_channels, device):
+        # Set image channels
+        if img_channels == 1:
+            mode = 'L'      # 8-bit pixels, grayscale
+        elif img_channels == 3:
+            mode = 'RGB'    # 8-bit RGB
+        else:
+            return False
+        
+        # Define a transform to convert a tensor to PIL image
+        transform = T.ToPILImage(mode=mode)
+        
+        # Generate and save images one at a time
+        for i in range(num_samples):
+            # Generate a single sample
+            with torch.no_grad():
+                # Create a single sample - you might need to modify create_generator_samples 
+                # or create a new method that generates one image at a time
+                if hasattr(self.train, 'create_single_generator_sample'):
+                    sample_tensor = self.train.create_single_generator_sample()
+                else:
+                    # Fallback: generate one sample using the existing method
+                    samples = self.train.create_generator_samples(1)
+                    sample_tensor = samples[0]
+            
+            # Process and save the single image
+            tensor = (sample_tensor + 1) / 2  # Normalize to [0, 1]
+            image_unflat = tensor.detach().cpu()
+            img = transform(image_unflat)
+            img_pth = f"{self.pth_samples}test_img_{i}.png"
+            img.save(img_pth)
+            
+            # Clear memory
+            del sample_tensor, tensor, image_unflat, img
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+            
+            # Progress update
+            if (i + 1) % 100 == 0:
+                print(f"Generated {i + 1}/{num_samples} images...")
+        
+        return True
+
     #############################################################################################################
     # CALL:
 
@@ -112,12 +157,11 @@ class Generate():
             self.load_checkpoint(self.train.netG, checkpoint_name, device)
             # Select a number of generated sample images
             num_samples = self.select_num_samples("Choose a number of sample images: ")
-            # Get image tensors
-            samples_tensors = self.train.create_generator_samples(num_samples)
-            # Plot and save all images as image single files (PIL Image)
-            self.save_sample_images(samples_tensors, setting["img_channels"])        
+            
+            # Use the new method that generates images one by one
+            self.generate_and_save_images_one_by_one(num_samples, setting["img_channels"], device)
+            
             print(f"{num_samples} sample images were successfully created and saved to folder {self.pth_samples}.")   
             return True
         else:
-            print("The checkpoint folder is empty!")     
-
+            print("The checkpoint folder is empty!")
